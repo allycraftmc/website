782341bdc19584b43b382004b66fbff761206336
Tert0 <62036464+Tert0@users.noreply.github.com>
2024-06-13 17:45:46 +0200

    made dark mode the default

--- themes/hugo-theme/assets/js/darkmode.js
+++ themes/hugo-theme/assets/js/darkmode.js
@@ -2,7 +2,7 @@ var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
 var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 
 // Change the icons inside the button based on previous settings
-if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage))) {
     themeToggleLightIcon.classList.remove('hidden');
 } else {
     themeToggleDarkIcon.classList.remove('hidden');
